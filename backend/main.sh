009241cee56900d4cb5d381234b08721b9430363
LuisDiazM <luismigueldiazmorales@gmail.com>
2022-01-10 22:40:29 -0500

    build add volumes and create superuser

--- backend/main.sh
+++ backend/main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python manage.py migrate
+sleep 2.5m
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('mario', 'luismiguel@radiogis.uis.edu.co', 'mario')" | python manage.py shell
 python manage.py collectstatic --no-input
+python manage.py migrate
 gunicorn --bind :$SERVER_PORT --workers 2 --threads 2 --timeout 10 radiogis_core.wsgi
