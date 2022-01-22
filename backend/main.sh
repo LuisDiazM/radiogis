835646c5908e3c23736be362f00259d49c5998e7
LuisDiazM <luismigueldiazmorales@gmail.com>
2022-01-21 21:56:45 -0500

    fix: update port 80 and create admin user after the migrations

--- backend/main.sh
+++ backend/main.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sleep 2.5m
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('mario', 'luismiguel@radiogis.uis.edu.co', 'mario')" | python manage.py shell
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('mario', 'devices@casiri.com', 'mario')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'luismiguel@radiogis.uis.edu.co', 'admin')" | python manage.py shell
 gunicorn --bind :$SERVER_PORT --workers 2 --threads 2 --timeout 10 radiogis_core.wsgi
